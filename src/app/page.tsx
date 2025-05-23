8bf6267ffe062cee0b019f53d559f79b0cab0d50
Germaine144 <umuhiregermaine12@gmail.com>
2025-05-23 13:51:05 +0200

    adding in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,7 @@ export default async function Home() {
       <Header />
       
       {/* Show this if user is signed out */}
+      
       <SignedOut>
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 flex items-center justify-center px-4 relative overflow-hidden">
           {/* Background decoration */}
